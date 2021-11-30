a2e1e4bfa9410217f47df4d42264710cc107285f
yrachid <anuar.yasser@gmail.com>
2021-11-30 20:44:38 -0300

    Fix test file not found bug

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 import * as vscode from "vscode";
 import path = require("path");
+import * as fs from "fs";
 
 export function activate(context: vscode.ExtensionContext) {
   const openTestFile = vscode.commands.registerCommand(
@@ -16,31 +17,39 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(openTestFileBeside);
 }
 
-const openFileCommand = (placement: vscode.ViewColumn) =>
-  async () => {
-    const activeEditor = vscode.window.activeTextEditor;
+const openFileCommand = (placement: vscode.ViewColumn) => async () => {
+  const activeEditor = vscode.window.activeTextEditor;
 
-    if (!activeEditor) {
-      vscode.window.setStatusBarMessage("go-to-test: No files open", 3000);
-      return;
-    }
+  if (!activeEditor) {
+    vscode.window.setStatusBarMessage("go-to-test: No files open", 3000);
+    return;
+  }
 
-    const testDocument = await testOf(activeEditor.document);
+  const testDocument = await testOf(activeEditor.document);
 
-    vscode.window.showTextDocument(testDocument, placement);
-  };
+  if (!testDocument) {
+    vscode.window.setStatusBarMessage("go-to-test: No test file found", 3000);
+    return;
+  }
+
+  vscode.window.showTextDocument(testDocument, placement);
+};
 
 async function testOf(
   currentFile: vscode.TextDocument
-): Promise<vscode.TextDocument> {
+): Promise<vscode.TextDocument | undefined> {
   const dirName: string = path.dirname(currentFile.uri.path);
   const fileName: string = path.basename(currentFile.uri.path);
 
-  const dot = fileName.lastIndexOf(".");
+  const lastDotIndex = fileName.lastIndexOf(".");
 
-  const testName = `${fileName.substring(0, dot)}.spec.ts`;
+  const testName = `${fileName.substring(0, lastDotIndex)}.spec.ts`;
 
   const testUri: vscode.Uri = vscode.Uri.file(path.join(dirName, testName));
+
+  if (!fs.existsSync(testUri.path)) {
+    return undefined;
+  }
 
   return await vscode.workspace.openTextDocument(testUri);
 }
